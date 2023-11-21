9e41c036eb21644447ce3bdcd2d9170cfd1d162c
sunjian <sujian_xm.cicdi@chinaccs.cn>
2023-11-21 08:46:36 +0800

    init

--- src/apis/user.js
+++ src/apis/user.js
@@ -3,17 +3,17 @@ const { request } = require("@/utils");
 
 // 1.登录请求
 export function loginAPI(formData) {
-   return request({
-        url: '/authorizations',
-        method: 'POST',
-        data: formData
-    })
+  return request({
+    url: "/authorizations",
+    method: "POST",
+    data: formData,
+  });
 }
 
 // 2,获取用户信息
 export function getProfileAPI() {
-    return request({
-        url: '/user/profile',
-        method: 'GET',
-    })
+  return request({
+    url: "/user/profile",
+    method: "GET",
+  });
 }
